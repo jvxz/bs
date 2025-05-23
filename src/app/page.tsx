7c1bf30206dea9948bcc077d6057f3f11918e097
jamie 🌨️ <jamie@wisp.bio>
2025-05-23 16:11:00 -0400

    refactor: index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+'use client'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Suspense } from 'react'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { useTRPC } from '@/lib/trpc/client'
 
 export default function Page() {
   return (
-    <div className="grid h-screen place-items-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
       <ThemeToggle />
+      <Suspense fallback={<p>loading from trpc...</p>}>
+        <TRPCData />
+      </Suspense>
     </div>
   )
+}
+
+function TRPCData() {
+  const trpc = useTRPC()
+  const { data } = useSuspenseQuery(trpc.hello.queryOptions())
+
+  return <p>{data as string}</p>
 }
