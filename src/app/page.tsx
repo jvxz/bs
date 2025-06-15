885513ceb801f1689c2e4841a9b65901eea28605
jamie 🌨️ <jamie@wisp.bio>
2025-06-15 19:06:04 -0400

    refactor: index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import { ThemeToggle } from '@/components/theme-toggle'
 import { client } from '@/lib/hono/client'
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
