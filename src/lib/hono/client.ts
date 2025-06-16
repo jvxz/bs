5e0b6f25849358978da0cee598c108a491e2239a
jamie 🌨️ <jamie@wisp.bio>
2025-06-16 12:24:40 -0400

    fix: rpc url

--- src/lib/hono/client.ts
+++ src/lib/hono/client.ts
@@ -1,6 +1,10 @@
 import { hc } from 'hono/client'
 import type { AppType } from '@/app/api/[...route]/route'
 
-export const client = hc<AppType>(
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
-)
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return ''
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  return 'http://localhost:3000'
+}
+
+export const client = hc<AppType>(getBaseUrl())
