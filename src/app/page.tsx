a2abb341026ed5cdb6e433bd9d595edfffa7c307
jamie 🌨️ <jamie@wisp.bio>
2025-07-17 14:07:37 -0400

    refactor: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { Suspense } from 'react'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { Spinner } from '@/components/ui/spinner'
 import { useTRPC } from '@/lib/trpc/client'
 
 export default function Page() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-2">
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
       <ThemeToggle />
-      <Suspense fallback={<p>loading from trpc...</p>}>
+      <Suspense fallback={<Spinner invert className="size-7" />}>
         <TRPCData />
       </Suspense>
     </div>
@@ -19,5 +20,5 @@ function TRPCData() {
   const trpc = useTRPC()
   const { data } = useSuspenseQuery(trpc.hello.queryOptions())
 
-  return <p>{data}</p>
+  return <pre>{data}</pre>
 }
