2f3956af07ac2848e9640b37ae669d6bd5fcf2d8
jamie 🌨️ <jamie@wisp.bio>
2025-07-17 14:07:27 -0400

    refactor: change font

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,11 +1,28 @@
-import { Geist_Mono, Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
 export const sans = Inter({
   subsets: ['latin'],
   variable: '--next-sans',
 })
 
-export const mono = Geist_Mono({
-  subsets: ['latin'],
+export const mono = localFont({
+  src: [
+    {
+      path: './fonts/TwilioSansMono-Medium.woff2',
+      style: 'normal',
+      weight: '500',
+    },
+    {
+      path: './fonts/TwilioSansMono-Regular.woff2',
+      style: 'normal',
+      weight: '400',
+    },
+    // {
+    //   path: './fonts/Writer-Bold.ttf',
+    //   style: 'bold',
+    //   weight: '700',
+    // },
+  ],
   variable: '--next-mono',
 })
