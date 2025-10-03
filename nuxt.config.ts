c6a8b6724b769aa98bcc43d4185f9e2d3792213a
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-03 15:42:29 -0400

    chore: init nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+
+  css: ['~/assets/css/globals.css'],
+
+  devtools: { enabled: true },
+
+  eslint: {
+    config: {
+      import: false,
+      standalone: false,
+    },
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
--- src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from 'next'
-import { name } from '../../package.json'
-
-export const meta: Metadata = {
-  description: `${name}'s description`,
-  title: name,
-}
