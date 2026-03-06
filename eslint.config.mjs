195b3ba6d572709882e9c314be92300810713a7a
jamie <hi@jamie.to>
2026-03-05 19:59:56 -0500

    chore: edit eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,41 +2,36 @@ import antfu from '@antfu/eslint-config'
 import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  antfu({
-    formatters: true,
-    plugins: {
-      'better-tailwindcss': eslintPluginBetterTailwindcss,
-    },
-    rules: {
-      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
-      'better-tailwindcss/enforce-consistent-line-wrapping': 'off',
-      'node/prefer-global/buffer': 'off',
-      // "style/arrow-parens": ['warn', 'as-needed'],
-      'node/prefer-global/process': 'off',
-      'perfectionist/sort-objects': 'warn',
-      'vue/html-self-closing': 'off',
-      'vue/max-attributes-per-line': [
-        'warn',
-        {
-          multiline: {
-            max: 1,
-          },
-          singleline: {
-            max: 2,
-          },
-        },
-      ],
-      'vue/no-empty-component-block': 'warn',
-      'vue/no-multiple-template-root': 'off',
-      'vue/sort-keys': 'warn',
-    },
-    settings: {
-      'better-tailwindcss': {
-        entryPoint: './app/assets/css/globals.css',
+export default withNuxt(antfu({
+  formatters: true,
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
+  },
+  rules: {
+    ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+    'antfu/curly': ['off'],
+    'better-tailwindcss/enforce-consistent-line-wrapping': 'off',
+    'curly': ['warn', 'multi-or-nest'],
+    'node/prefer-global/buffer': 'off',
+    'node/prefer-global/process': 'off',
+    'perfectionist/sort-objects': 'warn',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': ['warn', {
+      multiline: {
+        max: 1,
       },
+      singleline: {
+        max: 2,
+      },
+    }],
+    'vue/no-multiple-template-root': 'off',
+    'vue/sort-keys': 'warn',
+  },
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: './app/assets/css/globals.css',
     },
-    typescript: true,
-    vue: true,
-  }),
-)
+  },
+  typescript: true,
+  vue: true,
+}))
