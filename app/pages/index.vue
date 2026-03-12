7ad8cb6736e110e47c52e4cc29457db7d5e1971a
jamie <hi@jamie.to>
2026-03-12 13:42:36 -0400

    feat: make number endpoint example fail to provide error handling example

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,59 +2,75 @@
 const dialogOpen = ref(false)
 const timestamp = useTimestamp()
 
-const { data: randomNumber } = await useFetch('/api/number')
+const { data: randomNumber, error, refresh: fetchRandomNumber } = await useFetch('/api/number')
 </script>
 
 <template>
-  <Transition name="zoom" appear>
-    <div class="flex flex-col gap-2 h-screen items-center justify-center">
-      <ClientOnly>
-        <p class="tabular-nums">
-          {{ timestamp }}
-        </p>
-      </ClientOnly>
-      <p>{{ randomNumber }}</p>
-      <UDropdownMenuRoot>
-        <UDropdownMenuTrigger as-child>
-          <UButton>
-            Click!
+  <NuxtErrorBoundary>
+    <Transition name="zoom" appear>
+      <div class="flex flex-col gap-2 h-screen items-center justify-center">
+        <ClientOnly>
+          <p class="tabular-nums">
+            {{ timestamp }}
+          </p>
+        </ClientOnly>
+        <div class="flex gap-2 items-center">
+          <p v-if="error" class="text-danger">
+            {{ error.data?.message }}
+          </p>
+          <p v-else>
+            {{ randomNumber }}
+          </p>
+          <UButton
+            size="icon"
+            variant="ghost"
+            @click="fetchRandomNumber"
+          >
+            <Icon name="mingcute:refresh-1-line" />
           </UButton>
-        </UDropdownMenuTrigger>
-        <UDropdownMenuContent>
-          <UDropdownMenuItem @select="dialogOpen = true">
-            Dialog
-          </UDropdownMenuItem>
-        </UDropdownMenuContent>
-      </UDropdownMenuRoot>
-      <ClientOnly>
-        <UButton
-          size="icon"
-          variant="soft"
-          @click="toggleColorMode"
-        >
-          <Icon :name="$colorMode.value === 'dark' ? 'mingcute:sun-line' : 'mingcute:moon-line'" />
-        </UButton>
-      </ClientOnly>
-      <UDialogRoot v-model:open="dialogOpen">
-        <UDialogContent>
-          <UDialogHeader>
-            <UDialogTitle>
-              Example
-            </UDialogTitle>
-          </UDialogHeader>
-          <p class="font-sans">
-            This is sans
-          </p>
-          <p class="font-mono">
-            This is mono
-          </p>
-          <UDialogFooter>
-            <UDialogClose as-child>
-              <UButton>Close </UButton>
-            </UDialogClose>
-          </UDialogFooter>
-        </UDialogContent>
-      </UDialogRoot>
-    </div>
-  </Transition>
+        </div>
+        <UDropdownMenuRoot>
+          <UDropdownMenuTrigger as-child>
+            <UButton>
+              Click!
+            </UButton>
+          </UDropdownMenuTrigger>
+          <UDropdownMenuContent>
+            <UDropdownMenuItem @select="dialogOpen = true">
+              Dialog
+            </UDropdownMenuItem>
+          </UDropdownMenuContent>
+        </UDropdownMenuRoot>
+        <ClientOnly>
+          <UButton
+            size="icon"
+            variant="soft"
+            @click="toggleColorMode"
+          >
+            <Icon :name="$colorMode.value === 'dark' ? 'mingcute:sun-line' : 'mingcute:moon-line'" />
+          </UButton>
+        </ClientOnly>
+        <UDialogRoot v-model:open="dialogOpen">
+          <UDialogContent>
+            <UDialogHeader>
+              <UDialogTitle>
+                Example
+              </UDialogTitle>
+            </UDialogHeader>
+            <p class="font-sans">
+              This is sans
+            </p>
+            <p class="font-mono">
+              This is mono
+            </p>
+            <UDialogFooter>
+              <UDialogClose as-child>
+                <UButton>Close </UButton>
+              </UDialogClose>
+            </UDialogFooter>
+          </UDialogContent>
+        </UDialogRoot>
+      </div>
+    </Transition>
+  </NuxtErrorBoundary>
 </template>
--- server/api/number.get.ts
+++ server/api/number.get.ts
@@ -1,3 +1,12 @@
 export default defineEventHandler(async () => {
-  return `Your number was ${(Math.random() * 100).toFixed(2)}`
+  const number = Math.random()
+
+  if (number < 0.5) {
+    throw createError({
+      message: 'Number is less than 0.5',
+      status: 422,
+    })
+  }
+
+  return `Your number was ${number.toFixed(2)}`
 })
