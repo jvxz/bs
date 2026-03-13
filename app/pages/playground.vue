957c20abd59a283515e71127858b7acb670f518e
jamie <hi@jamie.to>
2026-03-13 15:20:01 -0400

    feat: add autocomplete component

---
+++ app/pages/playground.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape', 'Honeydew', 'Kiwi', 'Lemon', 'Lime', 'Mango', 'Melon', 'Nectarine', 'Orange', 'Papaya', 'Pear', 'Pineapple', 'Plum', 'Raspberry', 'Strawberry', 'Tangerine', 'Watermelon']
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <UAutocompleteRoot>
+      <UAutocompleteAnchor class="w-xs">
+        <UAutocompleteInput />
+        <UAutocompleteTrigger />
+        <UAutocompleteCancel />
+      </UAutocompleteAnchor>
+      <UAutocompleteContent>
+        <UAutocompleteViewport>
+          <UAutocompleteEmpty />
+          <UAutocompleteGroup>
+            <UAutocompleteItem
+              v-for="item in items"
+              :key="item"
+              :value="item"
+            >
+              {{ item }}
+            </UAutocompleteItem>
+          </UAutocompleteGroup>
+        </UAutocompleteViewport>
+      </UAutocompleteContent>
+    </UAutocompleteRoot>
+  </div>
+</template>
--- app/utils/styles.ts
+++ app/utils/styles.ts
@@ -1,32 +1,32 @@
 // @unocss-include
 const interactiveStyles = {
-  base: 'font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-danger/20 aria-invalid:border-danger dark:aria-invalid:ring-danger/40 shrink-0 gap-2 rounded text-base whitespace-nowrap font-medium outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 transition-transform duration-75',
+  base: 'aria-invalid:ring-danger/20 aria-invalid:border-danger dark:aria-invalid:ring-danger/40 text-base font-medium font-medium outline-none rounded underline-offset-4 shrink-0 gap-2 select-none whitespace-nowrap transition-transform duration-75 focus-visible:border-primary/50 disabled:opacity-50 [&_svg]:shrink-0 [&_svg]:size-4 [&_svg]:pointer-events-none disabled:pointer-events-none focus-visible:ring-[3px] active:ring-ring/60 focus-visible:ring-ring/40 hover:disabled:cursor-not-allowed',
   size: {
-    default: 'h-8 px-2.5 py-0.75 text-sm',
-    icon: 'aspect-square size-8',
-    lg: 'h-10 px-5 text-base text-lg',
-    sm: 'h-7 px-2.5 py-1 text-xs',
+    default: 'text-sm px-2.5 py-0.75 h-8',
+    icon: 'size-8 aspect-square',
+    lg: 'text-base text-lg px-5 h-10',
+    sm: 'text-xs px-2.5 py-1 h-7',
   },
   variant: {
     danger:
-      'hover:bg-danger/90 bg-danger/90 text-danger-foreground hover:bg-danger active:bg-danger/87.5 glow-red-500 border-danger',
+      'glow-red-500 text-danger-foreground border-danger bg-danger/90 active:bg-danger/87.5 hover:bg-danger hover:bg-danger/90',
     default:
-      'bg-primary/85 border border-primary active:bg-primary/80 text-primary-foreground glow-primary',
-    ghost: 'hover:bg-muted/90 active:bg-muted/75 text-muted-foreground hover:text-foreground',
+      'glow-primary text-primary-foreground border border-primary bg-primary/85 active:bg-primary/80',
+    ghost: 'text-muted-foreground hover:text-foreground active:bg-muted/75 hover:bg-muted/90',
     link: 'text-primary underline-offset-4 hover:underline',
     outline:
-      'border-border/90 hover:bg-muted/90 active:bg-muted/75 hover:border-border active:border-border border bg-transparent',
-    soft: 'border-border/90 text-muted-foreground hover:text-foreground hover:bg-muted/90 active:bg-muted/75 hover:border-border active:border-border border bg-input',
+      'border border-border/90 bg-transparent active:border-border hover:border-border active:bg-muted/75 hover:bg-muted/90',
+    soft: 'text-muted-foreground border border-border/90 bg-input hover:text-foreground active:border-border hover:border-border active:bg-muted/75 hover:bg-muted/90',
   },
 }
 
 const staticStyles = {
-  base: 'rounded p-5 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
+  base: 'p-5 rounded focus-visible:outline-none disabled:opacity-50 disabled:pointer-events-none',
   variant: {
     danger:
-      'bg-card border-danger text-danger border [&>svg]:text-current',
+      'text-danger border border-danger bg-card [&>svg]:text-current',
     default:
-      'bg-card border border-border text-card-foreground',
+      'text-card-foreground border border-border bg-card',
   },
 }
 
@@ -40,11 +40,11 @@ const popoverStyles = {
     interactiveStyles.base,
     interactiveStyles.variant.ghost,
     interactiveStyles.size.default,
-    'focus:bg-muted focus:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-12 data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:*:[svg]:!text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 relative flex cursor-default items-center p-1 px-2 text-sm outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+    'data-[highlighted]:bg-muted focus:bg-muted focus:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground [&_svg:not([class*=\'size-\'])]:size-12 data-[variant=danger]:text-danger-foreground data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:focus:text-danger-foreground data-[variant=danger]:*:[svg]:!text-danger-foreground dark:data-[variant=danger]:focus:bg-danger/40 relative flex cursor-default items-center p-1 px-2 text-sm outline-hidden transition-all select-none focus-visible:ring-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   ],
 }
 
 const overlayStyles
-  = 'duration-75 data-[state=open]:animate-in data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
+  = 'bg-overlay duration-75 inset-0 fixed z-50 backdrop-blur-xs data-[state=open]:animate-in data-[state=open]:fade-in-0'
 
 export { interactiveStyles, overlayStyles, popoverStyles, staticStyles }
